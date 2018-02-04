bf28f0cdfd7eaf53b0200b5d97203f20eab66b74
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-02-04 10:03:30 -0500

    Make API client

---
+++ Doggies/ApiClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace Doggies
+{
+    public class ApiClient
+    {
+        private HttpClient _client;
+
+        public ApiClient()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://dog.ceo/api/");
+        }
+
+        public async Task<IEnumerable<String>> GetMasterBreedsAsync()
+        {
+            var uri = new Uri("/api/breeds/list");
+
+            var response = await GetAsync(uri);
+
+            return response.Message;
+        }
+
+        private async Task<Response> GetAsync(Uri uri)
+        {
+            // Make the request
+            var response = await _client.GetAsync(uri);
+
+            // Throw an exception if the status code is not 200
+            response.EnsureSuccessStatusCode();
+
+            // Read the response as a stream. DataContractJsonSerializer requires
+            // a stream.
+            var contentStream = await response.Content.ReadAsStreamAsync();
+
+            return Deserialize(contentStream);
+        }
+
+        private Response Deserialize(Stream contentStream)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(Response));
+
+            return (Response)serializer.ReadObject(contentStream);
+        }
+    }
+}
---
+++ Doggies/Response.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Doggies
+{
+    [DataContract]
+    public class Response
+    {
+        [DataMember(Name = "status")]
+        public string Status { get; set; }
+
+        [DataMember(Name = "message")]
+        public string[] Message { get; set; }
+    }
+}
